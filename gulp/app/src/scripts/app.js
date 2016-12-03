8073f3ac13524014a67c8a1d893423f5e6f48560
wangzhenzhen <m18210214803_1@163.com>
2016-12-03 11:00:54 +0800

    zy

---
+++ gulp/app/src/scripts/app.js
@@ -0,0 +1,1 @@
+require("./demos/demo1.js");
---
+++ gulp/app/src/scripts/demos/demo1.js
@@ -0,0 +1,7 @@
+//引入common.js
+var commonUtil=require("../common/common");
+//引入html内容
+var demolTpl=require("../tpls/demo");
+console.log(demolTpl);
+//定义一个myApp的模块,没有[]表示获取模块
+//angular.module("myApp",[]);
